3084df07b7ece45a1f840df3eb20d1df1795e36e
sMorm <sereymorm@gmail.com>
2017-10-03 11:12:31 -0400

    Updated sell action

--- src/actions/sell.js
+++ src/actions/sell.js
@@ -2,22 +2,20 @@ import axios from 'axios'
 
 export function sell(data) {
   return dispatch => {
+    console.log(data.photoDescription)
     const formData = new FormData()
     data.photos.forEach((photo, index) => {
       formData.append('photos', photo, `photo${index}.png`)
     })
-    const textData = {
-      name: data.name,
-      photoDescription: JSON.stringify(data.photoDescription),
-      description: data.description,
-      brand: data.brand,
-      condition: data.condition,
-      userId: data.userId,
-      size: data.size,
-      category: data.category,
-      price: data.price
-    }
-    formData.append("data", JSON.stringify(textData))
+    formData.append('name', data.name)
+    formData.append('photoDescription', JSON.stringify(data.photoDescription))
+    formData.append('description', data.description)
+    formData.append('brand', data.brand)
+    formData.append('condition', data.condition)
+    formData.append('userId', data.userId)
+    formData.append('size', data.size)
+    formData.append('category', data.category)
+    formData.append('price', data.price)
     return axios.post('http://localhost:4000/api/listing', formData)
   }
 }
